12cb4ad96cbdf45c388c5f3ab7424254b6fabc69
Richard Wong <rwongsol@hotmail.com>
2022-12-10 22:39:19 -0500

    modificacion de ruta coneccion mysql

--- src/main/java/com/inventory/pe/model/Category.java
+++ src/main/java/com/inventory/pe/model/Category.java
@@ -1,4 +1,20 @@
 package com.inventory.pe.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+
 }
